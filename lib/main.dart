5d33ead1c2114eb31250d64f3acc63f9a1627a86
Hyun Jae Moon <hyunjaemoon@google.com>
2023-04-28 21:34:16 +0900

    Improved control flow for page index

--- lib/main.dart
+++ lib/main.dart
@@ -32,24 +32,36 @@ class _PortfolioMainPageState extends State<PortfolioMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Hyun Jae Moon'), actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () {
-              setState(() {
-                page = 0;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.note),
-            onPressed: () {
-              setState(() {
-                page = 1;
-              });
-            },
-          ),
-        ]),
-        body: page == 0 ? PortfolioHomePage() : ResumePage());
+      appBar: AppBar(title: Text("Moon Book"), actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () {
+            setState(() {
+              page = 0;
+            });
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.note),
+          onPressed: () {
+            setState(() {
+              page = 1;
+            });
+          },
+        ),
+      ]),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    switch (page) {
+      case 0:
+        return PortfolioHomePage();
+      case 1:
+        return ResumePage();
+      default:
+        return PortfolioHomePage();
+    }
   }
 }
