0ca0526a912bf5f055a93bf783d5fe6b2f855c1e
문현재 <hyunjaemoon@MacBook-Pro-2.local>
2024-05-27 15:23:54 -0700

    Fix Disclaimer Overflow

--- lib/translation_game/disclaimer.dart
+++ lib/translation_game/disclaimer.dart
@@ -4,19 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:moonbook/utils.dart';
 
 class DisclaimerWidget extends StatelessWidget {
+  const DisclaimerWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = min(MediaQuery.of(context).size.height, 800);
     return Container(
-      height: screenHeight * 0.15,
+      height: screenHeight * 0.2,
       color: Colors.grey[200],
       child: Center(
         child: Text(
-          '''Disclaimer: This app is created for the purpose of exploring and learning LLM. I will not be participating in the Gemini API Developer Competition.''',
-          style: fitTextStyle(context)
-              .apply(color: Colors.black, fontSizeFactor: 0.5),
+          'Disclaimer: This app is created for the purpose of exploring and learning LLM. I will not be participating in the Gemini API Developer Competition.',
+          style: fitTextStyle(context).apply(
+              color: Colors.black, fontSizeFactor: 0.5 * (750 / screenHeight)),
           textAlign: TextAlign.center,
-          maxLines: 2, // Set the maximum number of lines
+          maxLines: 5, // Set the maximum number of lines
           overflow: TextOverflow.ellipsis,
           softWrap: true,
         ),
